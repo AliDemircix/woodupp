7ce2a73a16743a381692e410e723e9a417320371
AliDemircix <ademirci@phinion.com>
2024-01-16 21:10:36 +0100

    Add middleDescription component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from './component/navbar';
 import ProductHero from './component/producthero/productHero';
 import Footer from './component/footer/footer';
 import StaticHeader from './component/header/staticHeader';
+import MiddleDescriptionText from './component/middleDescriptionText';
 export default function Home() {
   return (
     <main>
@@ -10,6 +11,7 @@ export default function Home() {
       <Navbar />
       <Container>
         <ProductHero />
+        <MiddleDescriptionText />
       </Container>
       <Footer />
     </main>
