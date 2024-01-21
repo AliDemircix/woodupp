f365a327ad024612a9ba664d4dced9dec7a47016
uludagaykut <aykut.uludag@fuga.com>
2024-01-21 12:48:32 +0100

    add links to the navbar

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,5 +1,29 @@
-function ProductDetailPage({ params }: { params: { slug: string } }) {
-  return <div>My Product: {params.slug}</div>;
+import { Container } from "@mui/material";
+import StaticHeader from "../../component/header/staticHeader";
+import Navbar from "../../component/navbar";
+import Footer from "../../component/footer/footer";
+import { products } from "../../lib/producthero/constants";
+
+const getData = async (slug: string) => {
+  const res = await products.find((product) => product.code === slug);
+
+  if (!res) {
+    throw new Error("Couldn't find product");
+  }
+
+  return res;
+};
+
+async function ProductDetailPage({ params }: { params: { slug: string } }) {
+  const product = await getData(params.slug);
+  return (
+    <div>
+      <StaticHeader />
+      <Navbar />
+      <Container maxWidth="xl">{product.name}</Container>
+      <Footer />
+    </div>
+  );
 }
 
 export default ProductDetailPage;
