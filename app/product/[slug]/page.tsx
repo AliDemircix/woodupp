a15de424d360287ab68ee37661e619fc75c3ac46
uludagaykut <aykut.uludag@fuga.com>
2024-01-22 19:19:13 +0100

    add product title, price and product cards

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,9 +1,10 @@
-import { Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import StaticHeader from "../../component/header/staticHeader";
 import Navbar from "../../component/navbar";
 import Footer from "../../component/footer/footer";
 import { products } from "../../lib/producthero/constants";
 import ProductDetailImageCarousel from "../../component/productDetail/ProductDetailImageCarousel";
+import ProductDetailCard from "../../component/productDetail/ProductDetailCard";
 
 export const getData = async (slug: string) => {
   const res = await products.find((product) => product.code === slug);
@@ -27,7 +28,30 @@ async function ProductDetailPage({ params }: { params: { slug: string } }) {
           <Grid item xs={12} sm={6}>
             <ProductDetailImageCarousel images={product.images} />
           </Grid>
-          <Grid item xs={12} sm={6}></Grid>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h4" mt={2}>
+              WoodUpp{" "}
+              <Typography
+                variant="h4"
+                component="span"
+                sx={{ fontWeight: "bold" }}
+              >
+                {product.name}
+              </Typography>
+            </Typography>
+            {/* We should retrieve this price from product data */}
+            <Typography variant="h4" mt={1}>
+              155 €
+            </Typography>{" "}
+            <Box my={2}>
+              <Typography variant="subtitle2">Maat:</Typography>
+              <Box>
+                {products.map((product) => (
+                  <ProductDetailCard key={product.id} product={product} />
+                ))}
+              </Box>
+            </Box>
+          </Grid>
         </Grid>
       </Container>
       <Footer />
