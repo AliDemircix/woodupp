9feae5b8cc94b524be20b595f77b603f0ca617b7
uludagaykut <aykut.uludag@fuga.com>
2024-02-03 13:22:32 +0100

    add product info section

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,62 +1,57 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
-import { products } from "../../lib/producthero/constants";
-import ProductDetailImageCarousel from "../../component/productDetail/ProductDetailImageCarousel";
-import ProductDetailCard from "../../component/productDetail/ProductDetailCard";
-import ProductDetailColorCard from "../../component/productDetail/ProductDetailColorCard";
+import { Box, Container, Grid, Typography } from '@mui/material';
+import { products } from '../../lib/producthero/constants';
+import ProductDetailImageCarousel from '../../component/productDetail/ProductDetailImageCarousel';
+import ProductDetailCard from '../../component/productDetail/ProductDetailCard';
+import ProductDetailColorCard from '../../component/productDetail/ProductDetailColorCard';
+import ProductInfoSection from '../../component/productDetail/ProductInfoSection';
 
 export const getData = async (slug: string) => {
-  const res = await products.find((product) => product.code === slug);
+    const res = await products.find((product) => product.code === slug);
 
-  if (!res) {
-    throw new Error("Couldn't find product");
-  }
+    if (!res) {
+        throw new Error("Couldn't find product");
+    }
 
-  return res;
+    return res;
 };
 
 async function ProductDetailPage({ params }: { params: { slug: string } }) {
-  const product = await getData(params.slug);
+    const product = await getData(params.slug);
 
-  return (
-    <Container maxWidth="xl" sx={{ py: 4 }}>
-      <Grid container spacing={4}>
-        <Grid item xs={12} sm={6}>
-          <ProductDetailImageCarousel productColors={product.colors} />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h4" mt={2}>
-            WoodUpp{" "}
-            <Typography
-              variant="h4"
-              component="span"
-              sx={{ fontWeight: "bold" }}
-            >
-              {product.name}
-            </Typography>
-          </Typography>
-          <Box my={2}>
-            <Typography variant="subtitle2">Maat:</Typography>
-            <Box>
-              {products.map((product) => (
-                <ProductDetailCard key={product.id} product={product} />
-              ))}
-            </Box>
-          </Box>
-          <Box my={2}>
-            <Typography variant="subtitle2">Kleuren:</Typography>
-            <Box sx={{ display: "flex", flexWrap: "wrap", gap: 1, my: 1 }}>
-              {product.colors.map((colorItem) => (
-                <ProductDetailColorCard
-                  productColor={colorItem}
-                  key={colorItem.name}
-                />
-              ))}
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </Container>
-  );
+    return (
+        <Container maxWidth="xl" sx={{ py: 4 }}>
+            <Grid container spacing={4}>
+                <Grid item xs={12} sm={6}>
+                    <ProductDetailImageCarousel productColors={product.colors} />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Typography variant="h4" mt={2}>
+                        WoodUpp{' '}
+                        <Typography variant="h4" component="span" sx={{ fontWeight: 'bold' }}>
+                            {product.name}
+                        </Typography>
+                    </Typography>
+                    <Box my={2}>
+                        <Typography variant="subtitle2">Maat:</Typography>
+                        <Box>
+                            {products.map((product) => (
+                                <ProductDetailCard key={product.id} product={product} />
+                            ))}
+                        </Box>
+                    </Box>
+                    <Box my={2}>
+                        <Typography variant="subtitle2">Kleuren:</Typography>
+                        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1, my: 1 }}>
+                            {product.colors.map((colorItem) => (
+                                <ProductDetailColorCard productColor={colorItem} key={colorItem.name} />
+                            ))}
+                        </Box>
+                    </Box>
+                    <ProductInfoSection description={product.description} />
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
 
 export default ProductDetailPage;
