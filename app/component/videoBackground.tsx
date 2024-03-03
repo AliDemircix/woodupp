40f53757c44a1ed642600e2520f1803bce6ad5fe
uludagaykut <aykut.uludag@fuga.com>
2024-03-03 14:59:58 +0100

    wip

--- app/component/videoBackground.tsx
+++ app/component/videoBackground.tsx
@@ -1,18 +1,30 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 export function VideoBackground() {
-  return (
-    <Box mt={2}>
-      <video
-        src={require('../../public/heroVideo.mp4')} // Update with your video file name
-        autoPlay
-        loop
-        muted
-        width="100%"
-        height="100%"
-        style={{ borderRadius: '24px' }}
-      />
-    </Box>
-  );
+    return (
+        <Box
+            my={3}
+            position="relative"
+            sx={{
+                height: 'calc(100vh - 144px)',
+            }}
+        >
+            <video
+                src={require('../../public/heroVideo.mp4')} // Update with your video file name
+                autoPlay
+                loop
+                muted
+                style={{ width: '100%', maxWidth: '100vw', height: '100%', borderRadius: '24px', objectFit: 'fill' }}
+            />
+            <Box position="absolute" sx={{ top: '75%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: 'white', width: '100%' }}>
+                <Typography variant="h5" sx={{ fontWeight: 'bold', mb: 1 }}>
+                    Elegantie voor een goedkope prijs
+                </Typography>
+                <Typography variant="h3" sx={{ fontWeight: 'bold' }}>
+                    Modern ontwerp en een esthetisch tintje
+                </Typography>
+            </Box>
+        </Box>
+    );
 }
