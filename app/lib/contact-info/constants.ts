fa74c03503d03f52ea845edfecd8d97117ce2d70
AliDemircix <ademirci@phinion.com>
2024-03-09 22:23:05 +0100

    Add email info

--- app/lib/contact-info/constants.ts
+++ app/lib/contact-info/constants.ts
@@ -1,5 +1,5 @@
 export const whatsapp = +31686202943;
 export const address = 'Energieweg 111 3641 RT Mijdrecht';
-export const email = ''; // TODO add email later
+export const email = 'ecdwoodwallnl.@hotmail.com';
 export const weeklyHours = ' Maandag t/m vrijdag: 8.00 – 17.00 uur';
 export const weekendHours = ' Weekend: Gesloten';
