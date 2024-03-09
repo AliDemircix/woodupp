9d49388a82eb407404f01782642e3d37433ee032
AliDemircix <ademirci@phinion.com>
2024-03-09 20:54:12 +0100

    Update FAQ title

--- app/customer/constants.ts
+++ app/customer/constants.ts
@@ -1,70 +1,70 @@
 export const constants = [
-  {
-    id: 'vaakgestelde-vragen-en-antworden',
-    title: 'VAAKGESTELDE VRAGEN',
-    questions: [
-      {
-        q: 'Wat zijn akupanelen?',
-        a: 'Akoestische panelen zijn wandpanelen die worden gebruikt om de akoestiek van een ruimte te verbeteren. Ze werken door geluid te absorberen en niet te weerkaatsen, wat resulteert in een betere geluidskwaliteit. Daarnaast kunnen ze ook dienen als decoratie, vanwege het brede scala aan kleuren en afwerkingen.',
-      },
-      {
-        q: 'Waar is het Akupanel van gemaakt?',
-        a: 'Het Akupanel bestaat uit een 9 mm. Akoestisch vilt waar de lamellen aan vast zitten. Het vilt is gemaakt van gerecycled plastic. De lamellen zijn gemaakt van MDF met een fineeroppervlak.',
-      },
-      {
-        q: 'Zijn de Akupanelen alleen voor binnen?',
-        a: 'Ja, de Akupanelen zijn alleen geschikt voor gebruik binnenshuis. De panelen raden wij niet aan in combinatie met water en vochtige ruimtes.',
-      },
-      {
-        q: 'Hoe werkt een akoestisch paneel?',
-        a: 'Akoestische panelen absorberen geluidstrillingen, waardoor het geluid niet terugkaatst en de ruimte een betere geluidskwaliteit krijgt. Het vilt achter de panelen zorgt voor de absorptie van de trillingen.',
-      },
-      {
-        q: 'Hoeveel akoestische panelen heb ik nodig?',
-        a: 'De breedte is 60 cm en de hoogtes zijn 240 cm en 300 cm dus het is afhankelijk van de breedte de oppervlakte. Onze akoestische panelen hebben een oppervlakte van 1,44 m² (60 * 240) en 1,80 m² (60 * 300) dus je kunt makkelijk berekenen hoeveel panelen je nodig hebt. Bereken de oppervlakte van de ruimte door de breedte te vermenigvuldigen met de hoogte.',
-      },
-      {
-        q: 'Kunnen de wandpanelen over tegels worden geplaatst?',
-        a: 'Ja, natuurlijk! Wij raden echter aan om ze met kit te bevestigen in plaats van te schroeven. Vooral bij sommige tegels kan schroeven tot speling leiden en de tegels later losmaken.',
-      },
-      {
-        q: 'Kan ik de panelen makkelijk zelf op maat maken?',
-        a: 'Voor het inkorten over de volledige breedte raden wij een cirkelzaag aan. Voor kleinere uitsneden raden wij een decoupeerzaag aan. Voor het versmallen van het paneel kan je met een scherp Stanley mes door het vilt snijden. Het is supper makkelijk toch!',
-      },
-      {
-        q: 'Hoe kan ik de randen van de panelen mooi afwerken?',
-        a: 'Het gebruik van onze eindlat kan je daar wel bij helpen. Daarbij zijn de panelen zeer eenvoudig te vermaken. Het vilt is namelijk goed te op maat te snijden met een scherp stanleymes.',
-      },
-      {
-        q: 'Kan het Akupaneel direct worden geplaatst of moet ik eerst een raamwerk plaatsen?',
-        a: 'Je kan de panelen eenvoudig en direct op een ondergrond monteren - een raamwerk is niet perse nodig.',
-      },
-      {
-        q: 'Hoe maak ik ruimte voor stopcontacten in het paneel?',
-        a: 'Als je stopcontacten aan de muur hebt, is de beste oplossing om een gat in de panelen te maken en de stopcontacten op het oppervlak van de akupanelen te installeren.',
-      },
-      {
-        q: 'Hoe maak ik de panelen schoon?',
-        a: 'Je kunt de panelen schoon houden met een stofdoek, een stofzuiger of een hard uitgewrongen doek.',
-      },
-      {
-        q: 'Kun je de panelen in een ronding aanbrengen?',
-        a: 'Ja, de panelen kunnen gebogen worden. Het vilt is gemaakt van gerecycled plastic en is flexibel, waardoor je de panelen om ronde hoeken kunt buigen.',
-      },
-      {
-        q: 'De hoogte van de muur is langer dan de lengte van het paneel - wat kan/moet ik doen?',
-        a: 'Heeft u een wand boven de 240 cm en 300 cm? Geen zorgen! De panelen kunnen eenvoudig op elkaar worden gekoppeld om de gewenste lengte te bereiken.',
-      },
-      {
-        q: 'Welk type schroef heb ik nodig voor installatie?',
-        a: 'Het type schroef dat je nodig is afhankelijk van het soort muur waarop de panelen worden gemonteerd. In sommige gevallen zijn pluggen nodig om de installatie lang mee te laten gaan. Het zou mooi zijn om zwart of grijze schroeven te bestellen die passen bij de kleur van het vilt.',
-      },
-      {
-        q: 'Op welke ondergronden kunnen de Akupanelen worden geïnstalleerd?',
-        a: 'Dat kan dat de akoestische panelen op bijna alle oppervlakken monteren, bakstenen en betonnen muren ook (de uitzondering is op een systeemplafond). Je kan ze aan het gewenste oppervlak lijmen of schroeven.',
-      },
-    ],
-  },
-  { id: 'montage', title: 'Montage' },
-  { id: 'contact', title: 'Contact' },
+    {
+        id: 'vaakgestelde-vragen-en-antworden',
+        title: 'VAAKGESTELDE VRAGEN EN ANTWOORDEN',
+        questions: [
+            {
+                q: 'Wat zijn akupanelen?',
+                a: 'Akoestische panelen zijn wandpanelen die worden gebruikt om de akoestiek van een ruimte te verbeteren. Ze werken door geluid te absorberen en niet te weerkaatsen, wat resulteert in een betere geluidskwaliteit. Daarnaast kunnen ze ook dienen als decoratie, vanwege het brede scala aan kleuren en afwerkingen.',
+            },
+            {
+                q: 'Waar is het Akupanel van gemaakt?',
+                a: 'Het Akupanel bestaat uit een 9 mm. Akoestisch vilt waar de lamellen aan vast zitten. Het vilt is gemaakt van gerecycled plastic. De lamellen zijn gemaakt van MDF met een fineeroppervlak.',
+            },
+            {
+                q: 'Zijn de Akupanelen alleen voor binnen?',
+                a: 'Ja, de Akupanelen zijn alleen geschikt voor gebruik binnenshuis. De panelen raden wij niet aan in combinatie met water en vochtige ruimtes.',
+            },
+            {
+                q: 'Hoe werkt een akoestisch paneel?',
+                a: 'Akoestische panelen absorberen geluidstrillingen, waardoor het geluid niet terugkaatst en de ruimte een betere geluidskwaliteit krijgt. Het vilt achter de panelen zorgt voor de absorptie van de trillingen.',
+            },
+            {
+                q: 'Hoeveel akoestische panelen heb ik nodig?',
+                a: 'De breedte is 60 cm en de hoogtes zijn 240 cm en 300 cm dus het is afhankelijk van de breedte de oppervlakte. Onze akoestische panelen hebben een oppervlakte van 1,44 m² (60 * 240) en 1,80 m² (60 * 300) dus je kunt makkelijk berekenen hoeveel panelen je nodig hebt. Bereken de oppervlakte van de ruimte door de breedte te vermenigvuldigen met de hoogte.',
+            },
+            {
+                q: 'Kunnen de wandpanelen over tegels worden geplaatst?',
+                a: 'Ja, natuurlijk! Wij raden echter aan om ze met kit te bevestigen in plaats van te schroeven. Vooral bij sommige tegels kan schroeven tot speling leiden en de tegels later losmaken.',
+            },
+            {
+                q: 'Kan ik de panelen makkelijk zelf op maat maken?',
+                a: 'Voor het inkorten over de volledige breedte raden wij een cirkelzaag aan. Voor kleinere uitsneden raden wij een decoupeerzaag aan. Voor het versmallen van het paneel kan je met een scherp Stanley mes door het vilt snijden. Het is supper makkelijk toch!',
+            },
+            {
+                q: 'Hoe kan ik de randen van de panelen mooi afwerken?',
+                a: 'Het gebruik van onze eindlat kan je daar wel bij helpen. Daarbij zijn de panelen zeer eenvoudig te vermaken. Het vilt is namelijk goed te op maat te snijden met een scherp stanleymes.',
+            },
+            {
+                q: 'Kan het Akupaneel direct worden geplaatst of moet ik eerst een raamwerk plaatsen?',
+                a: 'Je kan de panelen eenvoudig en direct op een ondergrond monteren - een raamwerk is niet perse nodig.',
+            },
+            {
+                q: 'Hoe maak ik ruimte voor stopcontacten in het paneel?',
+                a: 'Als je stopcontacten aan de muur hebt, is de beste oplossing om een gat in de panelen te maken en de stopcontacten op het oppervlak van de akupanelen te installeren.',
+            },
+            {
+                q: 'Hoe maak ik de panelen schoon?',
+                a: 'Je kunt de panelen schoon houden met een stofdoek, een stofzuiger of een hard uitgewrongen doek.',
+            },
+            {
+                q: 'Kun je de panelen in een ronding aanbrengen?',
+                a: 'Ja, de panelen kunnen gebogen worden. Het vilt is gemaakt van gerecycled plastic en is flexibel, waardoor je de panelen om ronde hoeken kunt buigen.',
+            },
+            {
+                q: 'De hoogte van de muur is langer dan de lengte van het paneel - wat kan/moet ik doen?',
+                a: 'Heeft u een wand boven de 240 cm en 300 cm? Geen zorgen! De panelen kunnen eenvoudig op elkaar worden gekoppeld om de gewenste lengte te bereiken.',
+            },
+            {
+                q: 'Welk type schroef heb ik nodig voor installatie?',
+                a: 'Het type schroef dat je nodig is afhankelijk van het soort muur waarop de panelen worden gemonteerd. In sommige gevallen zijn pluggen nodig om de installatie lang mee te laten gaan. Het zou mooi zijn om zwart of grijze schroeven te bestellen die passen bij de kleur van het vilt.',
+            },
+            {
+                q: 'Op welke ondergronden kunnen de Akupanelen worden geïnstalleerd?',
+                a: 'Dat kan dat de akoestische panelen op bijna alle oppervlakken monteren, bakstenen en betonnen muren ook (de uitzondering is op een systeemplafond). Je kan ze aan het gewenste oppervlak lijmen of schroeven.',
+            },
+        ],
+    },
+    { id: 'montage', title: 'Montage' },
+    { id: 'contact', title: 'Contact' },
 ];
