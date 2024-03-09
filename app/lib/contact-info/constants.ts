9cd090083f2f28e14ad4b51d415497a751f8a8d6
AliDemircix <ademirci@phinion.com>
2024-03-09 22:14:17 +0100

    Add contact info in constants

---
+++ app/lib/contact-info/constants.ts
@@ -0,0 +1,5 @@
+export const whatsapp = +31686202943;
+export const address = 'Energieweg 111 3641 RT Mijdrecht';
+export const email = ''; // TODO add email later
+export const weeklyHours = ' Maandag t/m vrijdag: 8.00 – 17.00 uur';
+export const weekendHours = ' Weekend: Gesloten';
